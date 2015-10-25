5cbbfc2c68cdcd5a7da10618b8f33eb2a7ad701a
Wayne Durack <wayne.durack@gmail.com>
2015-10-25 22:21:50 +0000

    Adding spread function

--- index.js
+++ index.js
@@ -3,24 +3,28 @@
 var concat = require('unique-concat')
 var omit = require('object-omit')
 
-module.exports = function createThenable (Promise, resolver) {
-  return methods(Promise).reduce(createMethod, {then: then})
-  function createMethod (thenable, name) {
-    thenable[name] = method(name)
-    return thenable
+module.exports = function createSpreadable (Promise, resolver) {
+  return methods(Promise).reduce(createMethod, {then: then, spread: spread})
+  function createMethod (spreadable, name) {
+    spreadable[name] = method(name)
+    return spreadable
   }
   function method (name) {
     return function () {
-      var promise = this.then()
+      var promise = this[name]()
       return promise[name].apply(promise, arguments)
     }
   }
-  function then (/*onFulfill, onReject*/) {
+  function then () {
     var promise = new Promise(resolver)
     return promise.then.apply(promise, arguments)
+  }
+  function spread () {
+    var promise = new Promise(resolver)
+    return promise.spread.apply(promise, arguments)
   }
 }
 
 function methods (Promise) {
-  return concat(['catch', 'finally'], Object.keys(omit(Promise.prototype, 'then')))
+  return concat(['catch', 'finally'], Object.keys(omit(Promise.prototype, 'then', 'spread')))
 }
