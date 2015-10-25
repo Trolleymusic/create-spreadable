f0c5c0289127ca2a41564d58c668cc053ab19b70
Wayne Durack <wayne.durack@gmail.com>
2015-10-25 22:21:50 +0000

    Adding spread function

--- test.js
+++ test.js
@@ -3,26 +3,62 @@
 var test = require('tape')
 var Bluebird = require('bluebird')
 var Q = require('q')
-var createThenable = require('./')
+var createSpreadable = require('./')
 
 test(function (t) {
-  t.test('Bluebird', function (t) {
-    return createThenable(Bluebird, function (resolve) {
+  t.test('Bluebird:then', function (t) {
+    return createSpreadable(Bluebird, function (resolve) {
       resolve('foo')
     })
-    .tap()
     .then(function (value) {
       t.equal(value, 'foo')
       t.end()
     })
   })
-  t.test('Q', function (t) {
-    return createThenable(Q.Promise, function (resolve) {
+  t.test('Bluebird:then multiple arguments', function (t) {
+    return createSpreadable(Bluebird, function (resolve) {
+      resolve(['foo', 'bar'])
+    })
+    .then(function (value) {
+      t.deepEqual(value, ['foo', 'bar'])
+      t.end()
+    })
+  })
+  t.test('Bluebird:spread', function (t) {
+    return createSpreadable(Bluebird, function (resolve) {
+      resolve(['foo', 'bar'])
+    })
+    .spread(function (first, second) {
+      t.equal(first, 'foo')
+      t.equal(second, 'bar')
+      t.end()
+    })
+  })
+  t.test('Q:then', function (t) {
+    return createSpreadable(Q.Promise, function (resolve) {
       resolve('foo')
     })
-    .finally(function () {})
     .then(function (value) {
       t.equal(value, 'foo')
+      t.end()
+    })
+  })
+  t.test('Q:then multiple arguments', function (t) {
+    return createSpreadable(Q.Promise, function (resolve) {
+      resolve(['foo', 'bar'])
+    })
+    .then(function (value) {
+      t.deepEqual(value, ['foo', 'bar'])
+      t.end()
+    })
+  })
+  t.test('Q:spread', function (t) {
+    return createSpreadable(Q.Promise, function (resolve) {
+      resolve(['foo', 'bar'])
+    })
+    .spread(function (first, second) {
+      t.equal(first, 'foo')
+      t.equal(second, 'bar')
       t.end()
     })
   })
